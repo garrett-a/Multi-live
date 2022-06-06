1abf08fde623119ba71784c1fb9728322a23c323
garrett-a <garrettaldridge@gmail.com>
2022-06-06 13:17:47 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import ReactTwitchEmbedVideo from "react-twitch-embed-video";
+
+import Card from "./Card";
+import "./App.css";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="grid">
+        {/* <ReactTwitchEmbedVideo
+          channel="ziqoftw"
+          width="100%"
+          targetId="first"
+          layout="video"
+        /> */}
+
+        <div className="wrapper">
+          <ReactPlayer
+            height="100%"
+            width="100%"
+            url="https://www.twitch.tv/ziqoftw"
+          />
+        </div>
+        <div className="wrapper">
+          <ReactPlayer url="https://www.twitch.tv/ziqoftw" />
+        </div>
+
+        <div className="chat">
+          <iframe
+            title="1"
+            id="twitch-chat-embed"
+            src="https://www.twitch.tv/embed/ziqoftw/chat?parent=localhost"
+            height="100%"
+            width="350"
+          ></iframe>
+        </div>
+
+        {/* <Card className="cards">
+          <ReactTwitchEmbedVideo
+            channel="richwcampbell"
+            width="100%"
+            targetId="second"
+            layout="video"
+          />
+        </Card> */}
+      </div>
+    </Fragment>
   );
 }
 
