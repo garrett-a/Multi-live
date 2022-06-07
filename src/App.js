3608b6efda553d6fa50a7d66590700db95debcb7
garrett-a <garrettaldridge@gmail.com>
2022-06-07 16:05:57 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ import Stream from "./components/Video/Stream";
 import Chat from "./components/Chat/Chat";
 import Container from "./UI/Container";
 
-import classes from "./UI/Container.module.css";
-
 function App() {
   return (
     <Fragment>
       <Header />
       <Container>
-        <Chat className={classes.chatContainer} />
-        <Stream className=".player1" />
+        <Chat />
+        <Stream />
       </Container>
     </Fragment>
   );
