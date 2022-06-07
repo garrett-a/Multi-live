2eaa6e97372d21c5ed4975652a5358741cc4ca34
garrett-a <garrettaldridge@gmail.com>
2022-06-07 13:12:10 -0500

    video layout change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { Fragment } from "react";
-import ReactTwitchEmbedVideo from "react-twitch-embed-video";
 
-import Card from "./Card";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Stream from "./components/Video/Stream";
 import Chat from "./components/Chat/Chat";
+import Container from "./UI/Container";
+
+import classes from "./UI/Container.module.css";
 
 function App() {
   return (
     <Fragment>
       <Header />
-      <Chat />
-      <div className="grid">
-        <Stream />
-      </div>
+      <Container>
+        <Chat className={classes.chatContainer} />
+        <Stream className=".player1" />
+      </Container>
     </Fragment>
   );
 }
--- src/components/Video/Stream.jsx
+++ src/components/Video/Stream.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import ReactPlayer from "react-player";
 
+import classes from "./Stream.module.css";
+
 const Stream = () => {
   return (
-    <div>
-      <ReactPlayer url="https://www.twitch.tv/ziqoftw" />
-    </div>
+    <Fragment>
+      <div className={classes.wrapper}>
+        <ReactPlayer
+          className={classes.player}
+          url="https://www.twitch.tv/ziqoftw"
+          width="100%"
+          height="100%"
+        />
+      </div>
+      <div className={classes.wrapper2}>
+        <ReactPlayer
+          className={classes.player}
+          url="https://www.twitch.tv/ziqoftw"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </Fragment>
   );
 };
 
