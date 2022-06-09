2fc3d01dd430d2e3fba69aae4978170a24d96d11
garrett-a <garrettaldridge@gmail.com>
2022-06-09 18:52:27 -0500

    integrated api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import classes from "./Header.module.css";
 
 const Header = (props) => {
+  useEffect(() => {
+    const fetchStreams = async () => {
+      const response = await fetch("https://api.twitch.tv/helix/streams", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+          "Client-Id": `owb00645opxcsak6j0dwv4w5ue7pcb`,
+          "Content-Type": "application/json",
+        },
+      });
+      const responseData = await response.json();
+      console.log(responseData);
+    };
+
+    fetchStreams();
+  }, []);
+
   const [open, setOpen] = useState(false);
 
   return (
     <header className={classes.header}>
       <h1>Twitch Player</h1>
       <input type="search"></input>
+
       <div>User</div>
       <i onClick={() => setOpen(!open)} class="fa-solid fa-bars">
         {open && props.children}
