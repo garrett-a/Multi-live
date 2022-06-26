dc3b3b816f9578e676313702d8090b13d30c82fe
garrett-a <garrettaldridge@gmail.com>
2022-06-26 12:11:26 -0500

    updated tabs

--- src/components/Chat/TabBar.jsx
+++ src/components/Chat/TabBar.jsx
@@ -16,6 +16,7 @@ export default function ScrollableTabsButtonForce() {
   };
 
   console.log(value);
+  console.log(...ctx.searchedStreams);
   console.log(ctx.searchedToChat);
 
   const darkTheme = createTheme({
