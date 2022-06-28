5351cccdaf222d64553890042d59d8ebfee312d3
garrett-a <garrettaldridge@gmail.com>
2022-06-28 12:20:01 -0500

    auth test

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -6,9 +6,11 @@ const HomePage = (props) => {
   const scope = encodeURIComponent("user:read:follows");
   const authHref = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=owb00645opxcsak6j0dwv4w5ue7pcb&redirect_uri=https://multi-live.netlify.app/&scope=${scope}`;
 
-  const parsedHash = window.location.hash.split("#")[1];
+  if (document.location.hash && document.location.hash != "") {
+    const parsedHash = window.location.hash.split("#")[1];
+    console.log(parsedHash);
+  }
 
-  console.log(parsedHash);
   return (
     <Fragment>
       <div className={classes.homepage}>
@@ -16,10 +18,7 @@ const HomePage = (props) => {
         <h3>Search a twitch stream.</h3>
         <h3>View up to four at once.</h3>
         <button>
-          <a
-            onClick={console.log(document.location.hash, parsedHash)}
-            href={authHref}
-          >
+          <a onClick={console.log(document.location.hash)} href={authHref}>
             Authorize
           </a>
         </button>
