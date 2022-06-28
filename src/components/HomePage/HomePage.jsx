807b2d52281bbea2db0b44c73abd5325ee7375f1
garrett-a <garrettaldridge@gmail.com>
2022-06-28 15:37:22 -0500

    fixed sccopes

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import React, { Fragment } from "react";
 import classes from "./HomePage.module.css";
 
 const HomePage = (props) => {
-  const scope = encodeURIComponent("user:read:follows+email");
+  const scope = encodeURIComponent("user:read:follows user:read:email");
   const authHref = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=owb00645opxcsak6j0dwv4w5ue7pcb&redirect_uri=https://multi-live.netlify.app/&scope=${scope}`;
 
   console.log(scope);
