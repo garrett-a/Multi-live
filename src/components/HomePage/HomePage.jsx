fdbb28686ba73a8da03bb96ac863eba80b2a445d
garrett-a <garrettaldridge@gmail.com>
2022-06-28 10:40:32 -0500

    auth test

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,8 +3,8 @@ import React, { Fragment } from "react";
 import classes from "./HomePage.module.css";
 
 const HomePage = (props) => {
-  // const scope = encodeURIComponent("user:read:follows");
-  // const authHref = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=owb00645opxcsak6j0dwv4w5ue7pcb&redirect_uri=https://localhost&scope=${scope}`;
+  const scope = encodeURIComponent("user:read:follows");
+  const authHref = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=owb00645opxcsak6j0dwv4w5ue7pcb&redirect_uri=https://multi-live.netlify.app&scope=${scope}`;
 
   return (
     <Fragment>
@@ -12,11 +12,11 @@ const HomePage = (props) => {
         <h2>Welcome to Multi-live.</h2>
         <h3>Search a twitch stream.</h3>
         <h3>View up to four at once.</h3>
-        {/* <button>
+        <button>
           <a onClick={console.log(document.location.hash)} href={authHref}>
             Authorize
           </a>
-        </button> */}
+        </button>
       </div>
     </Fragment>
   );
