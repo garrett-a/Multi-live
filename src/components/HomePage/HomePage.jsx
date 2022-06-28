5d9eae17cb2ad0849c23b718ce3f0771aeafa744
garrett-a <garrettaldridge@gmail.com>
2022-06-28 14:22:15 -0500

    fixed scopes

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,13 +3,10 @@ import React, { Fragment } from "react";
 import classes from "./HomePage.module.css";
 
 const HomePage = (props) => {
-  const scopeFollows = encodeURIComponent("user:read:follows");
-  const scopeEmail = encodeURIComponent("user:read:email");
-  const authHref = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=owb00645opxcsak6j0dwv4w5ue7pcb&redirect_uri=https://multi-live.netlify.app/&scope=${
-    scopeFollows + scopeEmail
-  }`;
+  const scope = encodeURIComponent("user:read:follows:email");
+  const authHref = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=owb00645opxcsak6j0dwv4w5ue7pcb&redirect_uri=https://multi-live.netlify.app/&scope=${scope}`;
 
-  console.log(authHref);
+  console.log(scope);
 
   return (
     <Fragment>
