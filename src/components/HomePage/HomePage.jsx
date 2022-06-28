4b1018003e7085a469fb56a14e4c40c2e717079c
garrett-a <garrettaldridge@gmail.com>
2022-06-28 12:24:15 -0500

    auth test

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -6,8 +6,8 @@ const HomePage = (props) => {
   const scope = encodeURIComponent("user:read:follows");
   const authHref = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=owb00645opxcsak6j0dwv4w5ue7pcb&redirect_uri=https://multi-live.netlify.app/&scope=${scope}`;
 
-  if (document.location.hash && document.location.hash != "") {
-    const parsedHash = window.location.hash.split("#")[1];
+  if (document.location.hash && document.location.hash !== "") {
+    const parsedHash = new URLSearchParams(window.location.hash.split("#")[1]);
     console.log(parsedHash);
   }
 
