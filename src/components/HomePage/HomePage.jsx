d63a0cea3c1c1c3f39489fda16b7f72290f13b01
garrett-a <garrettaldridge@gmail.com>
2022-06-29 18:18:38 -0500

    homepage update

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -6,8 +6,6 @@ const HomePage = (props) => {
   const scope = encodeURIComponent("user:read:follows user:read:email");
   const authHref = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=owb00645opxcsak6j0dwv4w5ue7pcb&redirect_uri=https://multi-live.netlify.app/&scope=${scope}`;
 
-  console.log(scope);
-
   return (
     <Fragment>
       <div className={classes.homepage}>
