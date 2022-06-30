b02fb88bb3fa679dcc99e1263fdd056af2b79efb
garrett-a <garrettaldridge@gmail.com>
2022-06-30 15:00:56 -0500

    homepage fix

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -47,8 +47,8 @@ const HomePage = () => {
           {isAuth && !ctx.isLoading && (
             <div>
               <span>
-                Hey <span>{ctx.userInfo.img}</span>
-                {ctx.userInfo.label}, happy viewing!
+                Hey <span>{ctx.userInfo[0].img}</span>
+                {ctx.userInfo[0].label}, happy viewing!
               </span>
             </div>
           )}
