ba402aa32e86cc2f12e6a55e5c527523828497e2
garrett-a <garrettaldridge@gmail.com>
2022-06-30 11:52:47 -0500

    auth fix

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -10,8 +10,11 @@ const HomePage = () => {
   const scope = encodeURIComponent("user:read:follows user:read:email");
   const authHref = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=owb00645opxcsak6j0dwv4w5ue7pcb&redirect_uri=https://multi-live.netlify.app/&scope=${scope}`;
 
-  const hashConfirm = window.location.hash.substring(1).split("=", [1]);
-  const errorConfirm = window.location.hash.substring(1).split("=", [1]);
+  const hashConfirm = window.location.hash.substring(1).split("=", [1]).shift();
+  const errorConfirm = window.location.hash
+    .substring(1)
+    .split("=", [1])
+    .shift();
   console.log(hashConfirm);
   console.log(errorConfirm);
 
